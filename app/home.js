48ad44d5af391bee55c997b3f6d22fb7ea405c3f
bosschill <59011325@kmitl.ac.th>
2020-02-02 22:16:33 +0700

    fixed home page

--- app/home.js
+++ app/home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 export default class App extends React.Component {
     render() {
